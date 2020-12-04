cacda242225ff781a760b18190d82562b3b892dc
Matthew Flatt <mflatt@racket-lang.org>
2020-12-04 07:10:13 -0700

    add AArch64 Mac OS dependency

--- draw-lib/info.rkt
+++ draw-lib/info.rkt
@@ -7,6 +7,7 @@
     ("draw-i386-macosx-3" #:platform "i386-macosx")
     ("draw-x86_64-macosx-3" #:platform "x86_64-macosx")
     ("draw-ppc-macosx-3" #:platform "ppc-macosx")
+    ("draw-aarch64-macosx-3" #:platform "aarch64-macosx")
     ("draw-win32-i386-3" #:platform "win32\\i386")
     ("draw-win32-x86_64-3" #:platform "win32\\x86_64")
     ("draw-x86_64-linux-natipkg-3" #:platform "x86_64-linux-natipkg")
