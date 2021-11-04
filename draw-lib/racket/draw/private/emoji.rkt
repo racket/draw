30ab1580624a6bd5fbb01c72c80332680cf2c407
Matthew Flatt <mflatt@racket-lang.org>
2021-11-04 08:07:27 -0600

    workaround absent drawing libraries on very old Mac OS
    Not having the libraries just means that emojis won't draw via
    `draw-text`.

--- draw-lib/racket/draw/private/emoji.rkt
+++ draw-lib/racket/draw/private/emoji.rkt
@@ -58,176 +58,183 @@
     [(eq? 'macosx (system-type))
 
      (define cf-lib (ffi-lib "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation"))
-     (define ct-lib (ffi-lib "/System/Library/Frameworks/CoreText.framework/CoreText"))
-     (define cg-lib (ffi-lib "/System/Library/Frameworks/CoreGraphics.framework/CoreGraphics"))
+     (define ct-lib (ffi-lib "/System/Library/Frameworks/CoreText.framework/CoreText" #:fail (lambda () #f)))
+     (define cg-lib (ffi-lib "/System/Library/Frameworks/CoreGraphics.framework/CoreGraphics" #:fail (lambda () #f)))
 
-     (define-ffi-definer define-cf cf-lib)
-     (define-ffi-definer define-ct ct-lib)
-     (define-ffi-definer define-cg cg-lib)
+     (cond
+       [(and ct-lib cg-lib)
+        (define-ffi-definer define-cf cf-lib)
+        (define-ffi-definer define-ct ct-lib)
+        (define-ffi-definer define-cg cg-lib)
 
-     (define _CFIndex _long)
+        (define _CFIndex _long)
 
-     (define-cpointer-type _CFDictionaryRef)
-     (define-cf CFDictionaryCreate (_fun (_pointer = #f) (_ptr i _pointer) (_ptr i _pointer) (_CFIndex = 1) (_pointer = #f) (_pointer = #f) -> _CFDictionaryRef))
+        (define-cpointer-type _CFDictionaryRef)
+        (define-cf CFDictionaryCreate (_fun (_pointer = #f) (_ptr i _pointer) (_ptr i _pointer) (_CFIndex = 1) (_pointer = #f) (_pointer = #f) -> _CFDictionaryRef))
 
-     (define-cpointer-type _CFAttributedStringRef)
-     (define-cf CFAttributedStringCreate (_fun (_pointer = #f) _NSString _CFDictionaryRef -> _CFAttributedStringRef))
+        (define-cpointer-type _CFAttributedStringRef)
+        (define-cf CFAttributedStringCreate (_fun (_pointer = #f) _NSString _CFDictionaryRef -> _CFAttributedStringRef))
 
-     (define-cf CFRelease (_fun _pointer -> _void))
+        (define-cf CFRelease (_fun _pointer -> _void))
 
 
-     (define _CGFloat (if (eqv? 64 (system-type 'word)) _double _float))
-     (define-cstruct _CGAffineTransform ([a _CGFloat]
-                                         [b _CGFloat]
-                                         [c _CGFloat]
-                                         [d _CGFloat]
-                                         [tx _CGFloat]
-                                         [ty _CGFloat]))
+        (define _CGFloat (if (eqv? 64 (system-type 'word)) _double _float))
+        (define-cstruct _CGAffineTransform ([a _CGFloat]
+                                            [b _CGFloat]
+                                            [c _CGFloat]
+                                            [d _CGFloat]
+                                            [tx _CGFloat]
+                                            [ty _CGFloat]))
 
-     (define-cpointer-type _CGFontRef)
-     (define-cg CGFontCreateWithFontName (_fun _NSString -> _CGFontRef)
-       #:wrap (allocator CFRelease))
+        (define-cpointer-type _CGFontRef)
+        (define-cg CGFontCreateWithFontName (_fun _NSString -> _CGFontRef)
+          #:wrap (allocator CFRelease))
 
-     (define-cpointer-type _CTFontRef)
-     (define-ct CTFontCreateWithGraphicsFont (_fun _CGFontRef _CGFloat (_pointer = #f) (_pointer = #f) -> _CTFontRef)
-       #:wrap (allocator CFRelease))
+        (define-cpointer-type _CTFontRef)
+        (define-ct CTFontCreateWithGraphicsFont (_fun _CGFontRef _CGFloat (_pointer = #f) (_pointer = #f) -> _CTFontRef)
+          #:wrap (allocator CFRelease))
 
-     (define _CGContextRef (_cpointer 'CGContextRef))
+        (define _CGContextRef (_cpointer 'CGContextRef))
 
-     (define-cg CGContextSaveGState (_fun _CGContextRef -> _void))
-     (define-cg CGContextRestoreGState (_fun _CGContextRef -> _void))
+        (define-cg CGContextSaveGState (_fun _CGContextRef -> _void))
+        (define-cg CGContextRestoreGState (_fun _CGContextRef -> _void))
 
-     (define-cg CGContextTranslateCTM (_fun _CGContextRef _CGFloat _CGFloat -> _void))
-     (define-cg CGContextScaleCTM (_fun _CGContextRef _CGFloat _CGFloat -> _void))
-     (define-cg CGContextConcatCTM (_fun _CGContextRef _CGAffineTransform -> _void))
+        (define-cg CGContextTranslateCTM (_fun _CGContextRef _CGFloat _CGFloat -> _void))
+        (define-cg CGContextScaleCTM (_fun _CGContextRef _CGFloat _CGFloat -> _void))
+        (define-cg CGContextConcatCTM (_fun _CGContextRef _CGAffineTransform -> _void))
 
-     (define-cg CGContextSetTextPosition (_fun _CGContextRef _CGFloat _CGFloat -> _void))
+        (define-cg CGContextSetTextPosition (_fun _CGContextRef _CGFloat _CGFloat -> _void))
 
-     (define-cpointer-type _CTLineRef)
-     (define-ct CTLineCreateWithAttributedString (_fun _CFAttributedStringRef -> _CTLineRef))
-     (define-ct CTLineDraw (_fun _CTLineRef _CGContextRef -> _void))
+        (define-cpointer-type _CTLineRef)
+        (define-ct CTLineCreateWithAttributedString (_fun _CFAttributedStringRef -> _CTLineRef))
+        (define-ct CTLineDraw (_fun _CTLineRef _CGContextRef -> _void))
 
-     (define-ct CTLineGetTypographicBounds (_fun _CTLineRef
-                                                 (a : (_ptr o _CGFloat))
-                                                 (d : (_ptr o _CGFloat))
-                                                 (l : (_ptr o _CGFloat))
-                                                 -> (w : _double)
-                                                 -> (values w a d l)))
+        (define-ct CTLineGetTypographicBounds (_fun _CTLineRef
+                                                    (a : (_ptr o _CGFloat))
+                                                    (d : (_ptr o _CGFloat))
+                                                    (l : (_ptr o _CGFloat))
+                                                    -> (w : _double)
+                                                    -> (values w a d l)))
 
-     (define-ct kCTFontAttributeName _pointer)
+        (define-ct kCTFontAttributeName _pointer)
 
-     (define emoji-cg (CGFontCreateWithFontName "Apple Color Emoji"))
+        (define emoji-cg (CGFontCreateWithFontName "Apple Color Emoji"))
 
-     (define emoji-ct-cache (make-hasheqv))
+        (define emoji-ct-cache (make-hasheqv))
 
-     (define emoji-bm-cache (make-weak-hash))
+        (define emoji-bm-cache (make-weak-hash))
 
-     (define (font->ct font)
-       (define size (exact->inexact (send font get-point-size)))
-       (or (hash-ref emoji-ct-cache size #f)
-           (let ([ct (CTFontCreateWithGraphicsFont emoji-cg size)])
-             (hash-set! emoji-ct-cache size ct)
-             ct)))
+        (define (font->ct font)
+          (define size (exact->inexact (send font get-point-size)))
+          (or (hash-ref emoji-ct-cache size #f)
+              (let ([ct (CTFontCreateWithGraphicsFont emoji-cg size)])
+                (hash-set! emoji-ct-cache size ct)
+                ct)))
 
-     (define (text->line font ch-or-str)
-       (define emoji-ct (font->ct font))
+        (define (text->line font ch-or-str)
+          (define emoji-ct (font->ct font))
 
-       (define str (if (string? ch-or-str)
-                       ch-or-str
-                       (string ch-or-str)))
+          (define str (if (string? ch-or-str)
+                          ch-or-str
+                          (string ch-or-str)))
 
-       (define font-dict (CFDictionaryCreate kCTFontAttributeName emoji-ct))
-       (define attr-str (CFAttributedStringCreate str font-dict))
-       (define line (CTLineCreateWithAttributedString attr-str))
-       (CFRelease attr-str)
-       (CFRelease font-dict)
+          (define font-dict (CFDictionaryCreate kCTFontAttributeName emoji-ct))
+          (define attr-str (CFAttributedStringCreate str font-dict))
+          (define line (CTLineCreateWithAttributedString attr-str))
+          (CFRelease attr-str)
+          (CFRelease font-dict)
 
-       line)
+          line)
 
-     ;; in atomic mode
-     (define (emoji-extent font ch-or-str)
-       (define line (text->line font ch-or-str))
-       (define-values (w a d l) (CTLineGetTypographicBounds line))
+        ;; in atomic mode
+        (define (emoji-extent font ch-or-str)
+          (define line (text->line font ch-or-str))
+          (define-values (w a d l) (CTLineGetTypographicBounds line))
 
-       (CFRelease line)
+          (CFRelease line)
 
-       ;; Dropping leading, because that's what Pango does
-       (values w (+ a d) d 0.0))
+          ;; Dropping leading, because that's what Pango does
+          (values w (+ a d) d 0.0))
 
-     (define (draw-emoji dc cr x y font ch-or-str)
-       (define line (text->line font ch-or-str))
-       (let draw-loop ([cr cr] [x x] [y y])
-         (cond
-           [(not (cairo_quartz_surface_get_cg_context (cairo_get_target cr)))
-            ;; Draw to a bitmap, then copy to cr
-            (define key (cons ch-or-str (send font get-point-size)))
-            (define bm+a (or (hash-ref emoji-bm-cache ch-or-str #f)
-                             (let ()
-                               (define-values (w a d l) (CTLineGetTypographicBounds line))
-                               (define bm (make-platform-bitmap (inexact->exact (ceiling w))
-                                                                (inexact->exact (ceiling (+ a d)))
-                                                                #:backing-scale 2.0))
-                               (send (send bm make-dc) in-cairo-context
-                                     (lambda (bm-cr)
-                                       (draw-loop bm-cr 0.0 a)))
-                               (define bm+a (cons bm a))
-                               (hash-set! emoji-bm-cache key bm+a)
-                               bm+a)))
-            (define bm (car bm+a))
-            (define ty (- y (cdr bm+a)))
-            (define w (send bm get-width))
-            (define h (send bm get-height))
-            (let ([old-s (cairo_get_source cr)])
-              (cairo_pattern_reference old-s)
-              (cairo_set_source_surface cr (send bm get-cairo-surface) x ty)
-              (let ([m (make-cairo_matrix_t 0.0 0.0 0.0 0.0 0.0 0.0)])
-                (cairo_matrix_init_translate m 0 0)
-                (cairo_matrix_scale m 2.0 2.0)
-                (cairo_matrix_translate m (- x) (- ty))
-                (cairo_pattern_set_matrix (cairo_get_source cr) m))
-              (cairo_pattern_set_filter (cairo_get_source cr) CAIRO_FILTER_NEAREST)
-              (cairo_new_path cr)
-              (cairo_rectangle cr x ty w h)
-              (cairo_fill cr)
-              (cairo_set_source cr old-s)
-              (cairo_pattern_destroy old-s))]
-           [else
-            ;; Convert Cairo transformation matrix to CG matrix
-            (define mx (make-cairo_matrix_t 1 0 0 1 0 0))
-            (cairo_get_matrix cr mx)
-            (define tm (make-CGAffineTransform (cairo_matrix_t-xx mx)
-                                               (cairo_matrix_t-yx mx)
-                                               (cairo_matrix_t-xy mx)
-                                               (cairo_matrix_t-yy mx)
-                                               (cairo_matrix_t-x0 mx)
-                                               (cairo_matrix_t-y0 mx)))
+        (define (draw-emoji dc cr x y font ch-or-str)
+          (define line (text->line font ch-or-str))
+          (let draw-loop ([cr cr] [x x] [y y])
+            (cond
+              [(not (cairo_quartz_surface_get_cg_context (cairo_get_target cr)))
+               ;; Draw to a bitmap, then copy to cr
+               (define key (cons ch-or-str (send font get-point-size)))
+               (define bm+a (or (hash-ref emoji-bm-cache ch-or-str #f)
+                                (let ()
+                                  (define-values (w a d l) (CTLineGetTypographicBounds line))
+                                  (define bm (make-platform-bitmap (inexact->exact (ceiling w))
+                                                                   (inexact->exact (ceiling (+ a d)))
+                                                                   #:backing-scale 2.0))
+                                  (send (send bm make-dc) in-cairo-context
+                                        (lambda (bm-cr)
+                                          (draw-loop bm-cr 0.0 a)))
+                                  (define bm+a (cons bm a))
+                                  (hash-set! emoji-bm-cache key bm+a)
+                                  bm+a)))
+               (define bm (car bm+a))
+               (define ty (- y (cdr bm+a)))
+               (define w (send bm get-width))
+               (define h (send bm get-height))
+               (let ([old-s (cairo_get_source cr)])
+                 (cairo_pattern_reference old-s)
+                 (cairo_set_source_surface cr (send bm get-cairo-surface) x ty)
+                 (let ([m (make-cairo_matrix_t 0.0 0.0 0.0 0.0 0.0 0.0)])
+                   (cairo_matrix_init_translate m 0 0)
+                   (cairo_matrix_scale m 2.0 2.0)
+                   (cairo_matrix_translate m (- x) (- ty))
+                   (cairo_pattern_set_matrix (cairo_get_source cr) m))
+                 (cairo_pattern_set_filter (cairo_get_source cr) CAIRO_FILTER_NEAREST)
+                 (cairo_new_path cr)
+                 (cairo_rectangle cr x ty w h)
+                 (cairo_fill cr)
+                 (cairo_set_source cr old-s)
+                 (cairo_pattern_destroy old-s))]
+              [else
+               ;; Convert Cairo transformation matrix to CG matrix
+               (define mx (make-cairo_matrix_t 1 0 0 1 0 0))
+               (cairo_get_matrix cr mx)
+               (define tm (make-CGAffineTransform (cairo_matrix_t-xx mx)
+                                                  (cairo_matrix_t-yx mx)
+                                                  (cairo_matrix_t-xy mx)
+                                                  (cairo_matrix_t-yy mx)
+                                                  (cairo_matrix_t-x0 mx)
+                                                  (cairo_matrix_t-y0 mx)))
 
-            ;; Extract CG context from Cairo, installing the Cairo
-            ;; clipping state as CG clipping
-            (define s (cairo_get_target cr))
-            (cairo_surface_flush s)
-            (define cg (cairo_quartz_get_cg_context_with_clip cr))
+               ;; Extract CG context from Cairo, installing the Cairo
+               ;; clipping state as CG clipping
+               (define s (cairo_get_target cr))
+               (cairo_surface_flush s)
+               (define cg (cairo_quartz_get_cg_context_with_clip cr))
 
-            ;; Set CG transformation to match Cairo plus displacement for glyph
-            (CGContextSaveGState cg)
-            (CGContextConcatCTM cg tm)
-            (CGContextTranslateCTM cg
-                                   (exact->inexact x)
-                                   (exact->inexact y))
-            (CGContextScaleCTM cg 1.0 -1.0)
+               ;; Set CG transformation to match Cairo plus displacement for glyph
+               (CGContextSaveGState cg)
+               (CGContextConcatCTM cg tm)
+               (CGContextTranslateCTM cg
+                                      (exact->inexact x)
+                                      (exact->inexact y))
+               (CGContextScaleCTM cg 1.0 -1.0)
 
-            ;; Actually draw the glpyh
-            (CGContextSetTextPosition cg 0.0 0.0)
-            (CTLineDraw line cg)
+               ;; Actually draw the glpyh
+               (CGContextSetTextPosition cg 0.0 0.0)
+               (CTLineDraw line cg)
 
-            (CFRelease line)
+               (CFRelease line)
 
-            ;; Go back to Cairo mode for the CG
-            (CGContextRestoreGState cg)
-            (cairo_quartz_finish_cg_context_with_clip cr)
-            (cairo_surface_mark_dirty s)])))
+               ;; Go back to Cairo mode for the CG
+               (CGContextRestoreGState cg)
+               (cairo_quartz_finish_cg_context_with_clip cr)
+               (cairo_surface_mark_dirty s)])))
 
-     (values emoji-extent draw-emoji)]
+        (values emoji-extent draw-emoji)]
+       [else
+        ;; can't draw emoji on old Mac OS
+        (values
+         (lambda args (values 12.0 12.0 12.0 0.0))
+         void)])]
     [else (values void void)]))
 
 ;; This submodule parses "emoji-sequences.txt" from Unicode
